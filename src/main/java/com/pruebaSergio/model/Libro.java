3503510b1af534abb2d846d3cf88abe63ab33820
SergioQuirantes <sergiolordi@hotmail.es>
2019-02-05 21:56:02 +0100

    Creado modelo

--- src/main/java/com/pruebaSergio/model/Libro.java
+++ src/main/java/com/pruebaSergio/model/Libro.java
@@ -1,9 +1,13 @@
 package com.pruebaSergio.model;
 
+import com.googlecode.objectify.annotation.Cache;
 import com.googlecode.objectify.annotation.Entity;
 import com.googlecode.objectify.annotation.Id;
+import com.wordnik.swagger.annotations.ApiModel;
 
 @Entity
+@Cache
+@ApiModel("Libro Object")
 public class Libro {
 
 	@Id
@@ -13,7 +17,49 @@ public class Libro {
 	
 	private String autor;
 	
+	private int anoPublicacion;
 	
+	private String genero;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	public int getAnoPublicacion() {
+		return anoPublicacion;
+	}
+
+	public void setAnoPublicacion(int anoPublicacion) {
+		this.anoPublicacion = anoPublicacion;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
 	
 	
 }
